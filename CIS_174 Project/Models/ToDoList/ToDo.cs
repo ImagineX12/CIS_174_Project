c13ce9bb678bbf0fc00523a992d13856cf67459b
ImagineX12 <86370767+ImagineX12@users.noreply.github.com>
2025-03-31 11:04:51 -0500

    Updated Data Validation on the ticket system

--- CIS_174 Project/Models/ToDoList/ToDo.cs
+++ CIS_174 Project/Models/ToDoList/ToDo.cs
@@ -7,16 +7,20 @@ namespace CIS_174_Project.Models.ToDoList
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Please enter a description.")]
+        [Required(ErrorMessage = "Please enter a name.")]
+        [MaxLength(50, ErrorMessage = "Name can't be over 50 characters long.")]
         public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter a description.")]
+        [MaxLength(500, ErrorMessage = "Description can't be over 500 characters long.")]
         public string Description { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter a sprint number.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Sprint number must be a positive number.")]
         public int SprintNumber { get; set; }
 
         [Required(ErrorMessage = "Please enter a point value.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Point value must be a positive number.")]
         public int PointValue { get; set; }
 
         [Required(ErrorMessage = "Please select a status.")]
